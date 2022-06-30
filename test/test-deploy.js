a1660658a305385d3ef28bd76dd8e1df14d17a91
AN <108299225+krakxn@users.noreply.github.com>
2022-06-30 19:50:19 +0530

    update

--- test/test-deploy.js
+++ test/test-deploy.js
@@ -1,17 +1,21 @@
 const { ethers } = require("hardhat")
 const { expect, assert } = require("chai")
 describe("SimpleStorage", () => {
-    let simpleStorage, simpleStorageFactory
+    let simpleStorage, simpleStorageFactory // For global scope
     beforeEach(async () => {
         simpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
         simpleStorage = await simpleStorageFactory.deploy()
     })
+    // More tests for a serious project
+    // Test 1
     it("Should start with a favourite number of 0", async () => {
         const currentValue = await simpleStorage.retrieve()
         const expectedValue = "0"
+        // `assert` and `expect`
         assert.equal(currentValue.toString(), expectedValue)
         // expect (currentValue.toString()).to.equal.(expectedValue)
     })
+    // Test 2
     it("Should update when we call store", async () => {
         const expectedValue = "7"
         const transactionResponse = await simpleStorage.store(expectedValue)

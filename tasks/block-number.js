c805206d703ec37e38b334158692fa9136ce2072
AN <108299225+krakxn@users.noreply.github.com>
2022-06-30 19:48:35 +0530

    update

--- tasks/block-number.js
+++ tasks/block-number.js
@@ -1,6 +1,6 @@
 const { task } = require("hardhat/config")
 task("block-number", "Prints the current block number").setAction(
-    async (taskArgs, hre) => {
+    async (taskArgs, hre) => { // HRE: Hardhat runtime environment
         const blockNumber = await hre.ethers.provider.getBlockNumber()
         console.log(`Current block number: ${blockNumber}`)
     }

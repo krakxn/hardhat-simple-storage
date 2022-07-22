6d05504922066b99eb40e884d8b927308644b6b9
AN <108299225+krakxn@users.noreply.github.com>
2022-07-22 19:05:25 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
-// Can lead to HH9 error if Hardhat is not initialized
-const { ethers, run, network } = require("hardhat")
+const { ethers, run, network } = require("hardhat") // Can lead to HH9 error if Hardhat is not initialized
+
 async function main() {
     const SimpleStorageFactory = await ethers.getContractFactory(
         "SimpleStorage"
@@ -8,21 +8,25 @@ async function main() {
     const simpleStorage = await SimpleStorageFactory.deploy()
     await simpleStorage.deployed() // Waits till it is deployed
     console.log(`Currently deployed to: ${simpleStorage.address}`)
+    
     // chainId for Goerli: 5, use Chain ID List for more info
     if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) { // Checks for the correct chainId and Etherscan's API
         console.log("Waiting for block TX(s)...")
         await simpleStorage.deployTransaction.wait(6) // Waits 6 blocks to confirm backend
         await verify(simpleStorage.address, [])
     }
+    
     // Get a number: (use `toString()` accordingly)
     const currentValue = await simpleStorage.retrieve()
     console.log(`Current value: ${currentValue}`)
+    
     // Update the number:
     const transactionResponse = await simpleStorage.store(7)
     await transactionResponse.wait(1) // Waits 1 block
     const updatedValue = await simpleStorage.retrieve()
     console.log(`Updated value: ${updatedValue}`)
 }
+
 // Function to verify contract on Etherscan
 const verify = async (contractAddress, args) => {
     console.log("Verifying...")
@@ -39,6 +43,7 @@ const verify = async (contractAddress, args) => {
         }
     }
 }
+
 main()
     .then(() => process.exit(0))
     .catch((error) => {

101f8d941058abdfde24ba69f3cf6e583cdd73f8
AN <108299225+krakxn@users.noreply.github.com>
2022-07-22 19:12:02 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,10 @@
-// const { hardhatArguments } = require("hardhat") -- Do not enable as it leads to a HH9 error
+// const { hardhatArguments } = require("hardhat") -- Could lead to a HH9 error on enabling
 require("@nomiclabs/hardhat-etherscan")
 require("@nomiclabs/hardhat-waffle")
 require("dotenv").config()
 require("./tasks/block-number")
 require("hardhat-gas-reporter")
 require("solidity-coverage")
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners()
-
-    for (const account of accounts) {
-        console.log(account.address)
-    }
-})
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -26,6 +14,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const CMC_API_KEY = process.env.CMC_API_KEY
 // const LOCALHOST_RPC_URL = process.env.LOCALHOST_RPC_URL
+
 module.exports = {
     defaultNetwork: "hardhat", // No need for --network hardhat but works either way even if you explicitly do it
     networks: {
@@ -36,7 +25,7 @@ module.exports = {
         },
         localhost: {
             url: "http://127.0.0.1:8545/",
-            // accounts:Thanks Hardhat!,
+            // accounts: Thanks Hardhat!,
             chainId: 31337,
         },
     },
